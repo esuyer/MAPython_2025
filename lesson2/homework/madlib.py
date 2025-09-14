8751e538988b302b973dda671c5bb2425587e2e4
Ed Suyer <62613484+esuyer@users.noreply.github.com>
2025-09-14 09:35:09 -0400

    Create madlib.py

---
+++ lesson2/homework/madlib.py
@@ -0,0 +1,5 @@
+name = input("Give me a name: ")
+adj = input("Give me an adjective: ")
+noun = input("Give me a noun: ")
+
+print("Please excuse", name, "who is far too", adj, "to attend", noun, "class")
