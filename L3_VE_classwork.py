fd1848760301efc356cc5c3d5109731fc701c4bc
vardhanemani2012-dot <vardhan.emani2012@gmail.com>
2025-10-31 00:29:52 +0000

    Lesson 3

---
+++ L3_VE_classwork.py
@@ -0,0 +1,9 @@
+#number=input("how many times you want to print hello world")
+#for i in range(int(number)):
+ #   print("hello world")
+#Integer=(int(input("give me an integer")))
+#print("previous:",Integer - 1)
+#print("Next:",Integer + 1)
+Integer=(int(input("give me an integer")))
+Integer2=(int(input("give me another integer")))
+print("sum:",Integer + Integer2)
---
+++ L3_ve_random.py
@@ -0,0 +1,12 @@
+import random
+import random 
+import turtle 
+
+turtle.setup(800, 600)
+t=turtle.Turtle()
+
+for i in range(4):
+  t.forward (50)
+  t.left(90)
+
+turtle.done()
