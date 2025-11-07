900f49fa907ae8f3d3b025fe60db0f0188abf7c2
Dragonslayer12Natsu <lukemarjault@gmail.com>
2025-11-07 00:07:51 +0000

    done

--- L3_LM_homework.py
+++ L3_LM_homework.py
@@ -1,4 +1,24 @@
+import turtle
+print("Question 1")
+print("")
 random=__import__("random")
-random.randint(1,10)
-random.randint(1,10)
-print(random.randint(1,10),random.randint(1,10))
+ran_num1=random.randint(1,10)
+ran_num2=random.randint(1,10)
+print(ran_num1,ran_num2)
+print(ran_num1+ran_num2)
+print(ran_num1-ran_num2)
+print("Question 2")
+print("")
+smile=int(input("How many smiley faces do you want?"))
+for i in range(smile):
+    print("\U0001f600")
+print("Question 3")
+print("")
+dice=(input("how many times do you want to roll the dice?"))
+
+
+for i in range(5):
+    random_number = random.randint(1, 6)
+    print(f"Random number {i+1}: {random_number}")
+print("Question 4")
+print("")
