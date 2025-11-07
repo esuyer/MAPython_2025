7aba2281bfbb8797f7948f3b998e419deb2e2843
Miniman <leon.aidelman7@gmail.com>
2025-11-06 20:21:55 -0500

    Create 4th.py

---
+++ 4th.py
@@ -0,0 +1,61 @@
+import time
+n = int(input("n is "))
+time.sleep(1)
+print("n > 10 is a", n > 10, "statement")
+time.sleep(1)
+print("n < 4 is a", n < 4, "statement")
+time.sleep(1)
+print("n = 15 is a", n == 15, "statement")
+time.sleep(1)
+print("n ≠ 10 is a", n != 10, "statement")
+time.sleep(1)
+if n <= 25:
+  print("n is less than or equal to 25")
+else:
+  print("n isn't less than or equal to 25")
+time.sleep(3)
+print("ok now its time for me to actually do the classwork...")
+time.sleep(2)
+num = int(input("give me a number please: "))
+if num == 12:
+  print("oh my god its a dozen... wow so cool (have you heard of this thing called sarcasm)")
+time.sleep(2)
+print("ok next")
+temp = int(input("how cold is it: "))
+if temp > 100:
+  print("what are u living in, an oven???")
+elif temp > 70:
+  print("go to the beach or smthn like that")
+elif temp > 55:
+  print("go touch some grass or make some real friends instead of talking to a digital robot with no emotions, its the perfect time to go out!")
+elif temp > 32:
+  print("ok, at least you arent living in an oven or a pool of liquid nitrogen")
+elif temp > 0:
+  print("its kinda chilly, just stay inside and play roblox or whatever you do when you arent touching grass or talking to friends, if you have any...")
+elif temp > -30:
+  print("lemme guess: ur canadian")
+elif temp > -460:
+  print("how are you alive... no jacket or blanket or human sized microwave could thaw you")
+else:
+  print("thats impossibe, absolute zero is about -460, and you put", temp, "so you dont care about the laws of the universe i guess, ehh dont care tho")
+time.sleep(4)
+print("next")
+time.sleep(2)
+print("ya kno what, ima skip it, not enough input from u")
+time.sleep(0.5)
+print("its basically about the week and what day it is but ima skip it...")
+time.sleep(2)
+n = int(input("Necesito una número"))
+if n % 5 == 0:
+  print("dat number is divisible by 5")
+else:
+  print("dat number isnt divisible by 5")
+print("not like i care")
+n = int(input("Necesito una número dos"))
+if n % 2 == 0:
+  print("dat number is even")
+else:
+  print("dat number is odd")
+print("again, not like i care")
+time.sleep(3)
+print("not donee")
