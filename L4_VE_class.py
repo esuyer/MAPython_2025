afa8f13aa17c842429e38d99b2eadbf9c8d6504a
vardhanemani2012-dot <vardhan.emani2012@gmail.com>
2025-11-07 00:42:54 +0000

    Classwork

---
+++ L4_VE_class.py
@@ -0,0 +1,13 @@
+import turtle
+import random
+
+t= turtle.Turtle()
+t.speed(5)
+
+for i in range(5):
+  side= random.randint(50,200)
+  for i in range(3):
+    t.forward(side)
+    t.left(120)
+
+turtle.done()
