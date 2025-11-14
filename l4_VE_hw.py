aa1e18531fff44542b27825a87250dd529b3d935
vardhanemani2012-dot <vardhan.emani2012@gmail.com>
2025-11-14 00:00:30 +0000

    Final lesson 4 homework

--- l4_VE_hw.py
+++ l4_VE_hw.py
@@ -1,4 +1,11 @@
-Int1=input("give me an integer")
-print("integer 1 is", Int1)
-Int2=input("give me another integer")
-print("integer 2 is", Int2)
+Int1=input("give me an integer: ")
+#print("integer 1 is", Int1)
+Int2=input("give me another integer: ")
+#print("integer 2 is", Int2)
+if Int1>Int2:
+  print(Int2)
+else:
+   print(Int1)
+
+
+
