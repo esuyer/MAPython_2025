7525f45cddccb4508e4db73025fca1bf0eaa480a
Dragonslayer12Natsu <lukemarjault@gmail.com>
2025-11-14 01:27:23 +0000

    done

---
+++ L5_LM_challenge.py
@@ -0,0 +1,26 @@
+import random
+
+#print("Question 3")
+#for i in range(10):
+#  x=random.randint(1,10)
+#  if x  == 1:
+#    print("First in line!")
+#  elif x == 2:
+#    print("Pair")
+#  elif x == 5:
+#     print("High five!")
+#  elif x == 7:
+#     print("Lucky seven!")
+#  else  :
+#     print(x)
+#print("Question 1")
+#Num1= int(input("Give me a number"))
+#if Num1>=90:
+#    print("Grade A")
+#elif Num1>=80 and Num1<=89:
+#    print("Grade B")
+#elif Num1>=70 and Num1<=79:
+#    print("Grade C")
+#else  :
+#    print("You need to retake the test.")
+#print("Question 4")
