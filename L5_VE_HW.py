544d71f4d44a27bd4665b81eac1ba218baabdde6
vardhanemani2012-dot <vardhan.emani2012@gmail.com>
2025-11-16 23:20:41 +0000

    L5 homework final

--- L5_VE_HW.py
+++ L5_VE_HW.py
@@ -1,5 +1,18 @@
 money=int(input("Mr.Blob how much money did you get this year"))
-if money>=70000:
-  print("You have to pay",70000-money/4, "in taxes")
-else:
-  ("you owe nothing in taxes")
+if money>70000:
+   print("You have to pay",(money-70000)/4, "in taxes")
+
+
+print("problem 2")
+
+age=int(input("what is your age"))
+if age<3:
+   print("you are a baby")
+if age>=3 and age<=9:
+  print("you are a kid")
+if age>=10 and age<=12:
+print("you are a preteen")
+if age>=13 and age<=17
+print("you are a teen")
+if age>18:
+  print("you are an adult")
