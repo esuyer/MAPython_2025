97ea06d80be628d259259da7946f07134b94550f
vardhanemani2012-dot <vardhan.emani2012@gmail.com>
2025-11-16 23:32:40 +0000

    L5_VE

--- L5_VE_HW.py
+++ L5_VE_HW.py
@@ -1,6 +1,8 @@
 money=int(input("Mr.Blob how much money did you get this year"))
 if money>70000:
    print("You have to pay",(money-70000)/4, "in taxes")
+if money<70000:
+   print("you owe no taxes")
 
 
 print("problem 2")
@@ -8,11 +10,18 @@ print("problem 2")
 age=int(input("what is your age"))
 if age<3:
    print("you are a baby")
-if age>=3 and age<=9:
-  print("you are a kid")
-if age>=10 and age<=12:
-print("you are a preteen")
-if age>=13 and age<=17
-print("you are a teen")
-if age>18:
-  print("you are an adult")
+else:
+  if age>=3 and age<=9:
+    print("you are a kid")
+  else:
+    if age>=10 and age<=12:
+      print("you are a preteen")
+    else:
+      if age>=13 and age<=17:
+        print("you are a teen")
+      else:
+        if age>18:
+          print("you are an adult")
+        else:
+          if age>65:
+            print("you are a senior citizen")
