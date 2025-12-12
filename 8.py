8f1dfa762b132308e2a457bb755fdfdd9cdaa262
Miniman <leon.aidelman7@gmail.com>
2025-12-11 20:16:59 -0500

    Create 8.py

---
+++ 8.py
@@ -0,0 +1,66 @@
+import random
+'''def hw(language):
+  if language == "english":
+    print("hello world")
+  elif language == "spanish":
+    print("hola mundo")
+def ohyeawasthissupposedtobecalledgreetuhokyeaitwassupposedtobecalledgreetsoiwillsaygreetuhgreet(name):
+  print("helo,", name)
+ohyeawasthissupposedtobecalledgreetuhokyeaitwassupposedtobecalledgreetsoiwillsaygreetuhgreet("bob")
+ohyeawasthissupposedtobecalledgreetuhokyeaitwassupposedtobecalledgreetsoiwillsaygreetuhgreet("james")
+ohyeawasthissupposedtobecalledgreetuhokyeaitwassupposedtobecalledgreetsoiwillsaygreetuhgreet("leon")
+ohyeawasthissupposedtobecalledgreetuhokyeaitwassupposedtobecalledgreetsoiwillsaygreetuhgreet("ben")
+ohyeawasthissupposedtobecalledgreetuhokyeaitwassupposedtobecalledgreetsoiwillsaygreetuhgreet("marcello")
+ohyeawasthissupposedtobecalledgreetuhokyeaitwassupposedtobecalledgreetsoiwillsaygreetuhgreet("luke")
+ohyeawasthissupposedtobecalledgreetuhokyeaitwassupposedtobecalledgreetsoiwillsaygreetuhgreet("lord of time, space, and lemons")
+# this was for classwork
+'''
+def fish(length):
+  print("}<^"+"«"*length+"€ᐷ")
+fish(4)
+fish(7)
+fish(2)
+fish(100)
+def repitan(speak):
+  if random.randint(1,100) == 1:
+    print("squak (I refuse to accept you forcing me to speak your dialect, instead I shall speak as how i would use my voice. squak)")
+  else:
+    print("squak squak", speak)
+repitan("nice bird")
+repitan("eee")
+repitan("i dont know what to say so i shall let you speak: squak")
+def tailtale(weight):
+  print("once upon a time, i caught a fish who weight an astonishing", weight, "kilos (or approximatley", weight * 2.205, "american weights")
+tailtale(2)
+tailtale(66666.666666666666666666667)
+tailtale(0.1)
+def ntothen(n):
+  if n >= 25:
+    print("im too lazy to do", n+"^"+n, "and im also to lazy to tell you that", n+"^"+n, "is", n**n)
+  else:
+    print(n**n)
+ntothen(2)
+ntothen(15)
+ntothen(67)
+def makeitbeeg(n):
+  return n*1000000
+print(makeitbeeg(3))
+print(makeitbeeg(254))
+print(makeitbeeg(832173219773211474321989810818798798743.8398329))
+def triange(h,w):
+  return h * w * 0.5
+print(triange(3,4))
+print(triange(1,7))
+print(triange(7,9))
+print(triange(12,2))
+print(triange(0.0001,100000))
+def fishes(length, amount):
+  for i in range(amount):
+    print("}<"+"«"*length+"€ᐷ")
+fishes(4,2)
+fishes(3,5)
+fishes(1,1)
+fishes(2,1)
+fishes(3,1)
+fishes(4,5)
+print("not doen")
