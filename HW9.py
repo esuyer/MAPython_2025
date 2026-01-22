3e37cb16dd8643d88d8ee99964fc90edbc5c198f
BCNHG <benmarjault@gmail.com>
2026-01-22 23:28:05 +0000

    Stuff

--- HW9.py
+++ HW9.py
@@ -1,19 +1,52 @@
-import turtle
+print("Problem 1")
+word = input("Give me a word: ")
+for letter in word:
+    print(letter)
 
-def draw_grid(N, L):
-    t = turtle.Turtle()
-    t.speed(0)
-    for i in range(N + 1):
-        t.penup()
-        t.goto(0, i * L)
-        t.pendown()
-        t.forward(N * L)
-    t.left(90)
-    for i in range(N + 1):
-        t.penup()
-        t.goto(i * L, 0)
-        t.pendown()
-        t.forward(N * L)
-    turtle.done()
+print("Problem 2")
+word = input("Give me a word: ")
+for letter in word:
+    print(letter * 5)
 
-draw_grid(5, 100)
+print("Problem 3")
+word = input("Give me a word: ")
+vowels = "aeiou"
+count = 0
+for letter in word:
+    if letter in vowels:
+        print(letter)
+        count += 1
+print(count, "vowels")
+
+print("Problem 4")
+word = input("Give me a word: ")
+vowels = "aeiou"
+for letter in word:
+    if letter not in vowels:
+        print(letter)
+
+print("Problem 5")
+word = input("Give me a word: ")
+for letter in word:
+    if letter == 'a':
+        print('z')
+    else:
+        print(letter)
+
+print("Problem 6")
+word = input("Give me a word: ")
+for i in range(len(word)):
+    if i % 2 == 0:
+        print(word[i])
+    else:
+        print('*')
+
+print("Problem 7")
+word = input("Give me a word: ")
+for i in range(len(word)):
+    print(word[i] * (i + 1))
+
+print("Problem 8")
+word = input("Give me a word: ")
+for letter in word[::-1]:
+    print(letter)
