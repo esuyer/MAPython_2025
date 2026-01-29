ccfe4b05da63f40c677dca11af3a8be033cbc059
BCNHG <benmarjault@gmail.com>
2026-01-29 23:49:25 +0000

    hw

--- HW9.py
+++ HW9.py
@@ -1,52 +1,57 @@
-print("Problem 1")
-word = input("Give me a word: ")
-for letter in word:
-    print(letter)
+def problem1():
+    print("Problem 1")
+    print()
+    word = input("Give me a word: ")
+    for c in word:
+        if c == 'a' or c == 'A':
+            print(c)
 
-print("Problem 2")
-word = input("Give me a word: ")
-for letter in word:
-    print(letter * 5)
-
-print("Problem 3")
-word = input("Give me a word: ")
-vowels = "aeiou"
-count = 0
-for letter in word:
-    if letter in vowels:
-        print(letter)
-        count += 1
-print(count, "vowels")
-
-print("Problem 4")
-word = input("Give me a word: ")
-vowels = "aeiou"
-for letter in word:
-    if letter not in vowels:
-        print(letter)
-
-print("Problem 5")
-word = input("Give me a word: ")
-for letter in word:
-    if letter == 'a':
-        print('z')
+def chain(w1, w2):
+    print("Problem 2")
+    print()
+    if w1[0] == w2[-1]:
+        print(w2, w1)
+    elif w2[0] == w1[-1]:
+        print(w1, w2)
     else:
-        print(letter)
+        print("NO CHAIN")
 
-print("Problem 6")
-word = input("Give me a word: ")
-for i in range(len(word)):
-    if i % 2 == 0:
-        print(word[i])
+def problem3():
+    print("Problem 3")
+    print()
+    w1 = input("Give me a word: ")
+    w2 = input("Give me another word: ")
+    if w1[-2:] == w2[-2:]:
+        print(w1, "and", w2, "rhyme")
     else:
-        print('*')
+        print(w1, "and", w2, "DO NOT rhyme")
 
-print("Problem 7")
-word = input("Give me a word: ")
-for i in range(len(word)):
-    print(word[i] * (i + 1))
+def secret_message(a, b):
+    print("Problem 4")
+    print()
+    for i in range(len(a)):
+        print(a[i])
+        print(b[i])
 
-print("Problem 8")
-word = input("Give me a word: ")
-for letter in word[::-1]:
-    print(letter)
+def problem5():
+    print("Problem 5")
+    print()
+    word = input("Give me a word: ")
+    for i in range(len(word)):
+        print("." * i + word[i])
+
+problem1()
+print()
+chain('dog','toad')
+print()
+chain('cat','take')
+print()
+chain('cake','rat')
+print()
+problem3()
+print()
+secret_message('Pto','yhn')
+print()
+secret_message('fo','lp')
+print()
+problem5()
