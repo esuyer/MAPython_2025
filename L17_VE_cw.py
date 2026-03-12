6d2fd96b75c8bd6a59fc2fe479c39374cb83de31
vardhanemani2012-dot <vardhan.emani2012@gmail.com>
2026-03-12 23:01:07 +0000

    hw

---
+++ L17_VE_cw.py
@@ -0,0 +1,68 @@
+import random
+#for i in range(4):
+ # print("hello",i+1)
+
+#s="Python"
+#for i in s:
+ #  print("Hi", i)
+
+
+
+
+#i=33
+#for i in range(4):
+ # i=20
+ # print(i)
+ # i=1000
+
+
+#i=0
+#while i<4:
+#  print("hello",i+1)
+#  i +=1
+
+
+#money=int(input("How much money do you have? "))
+
+#while money>0:
+#  price=random.randint(5,25)
+#  print(price,"price")
+#  money=money-price
+#  print("got it still have $",money)
+
+#print("no more money")
+  
+  
+#i=0
+#while i<11:
+#   print(i)
+#   i+=1
+
+
+#n=5
+#while n<13:
+#   print(n)
+#   n+=1
+
+#t=5
+#while t>-6:
+#   print(t)
+#   t-=1
+
+
+#N=int(input("Enter a number less that 50: "))
+
+#while N<51:
+#  print(N)
+#  N+=2
+
+#number=int(input("Enter a positive number: "))
+#while number>-1:
+#   print(number)
+#   number-=3
+
+int1=int(input("Enter a number: "))
+int2=int(input("Enter another bigger number: "))
+
+while int1 + 1 <int2:
+  
