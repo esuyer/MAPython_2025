709786a3592cd1ba0154bef38091ab06345225d0
Ed Suyer <62613484+esuyer@users.noreply.github.com>
2026-04-10 00:10:08 +0000

    22

--- L22.py
+++ L22.py
@@ -4,26 +4,75 @@ if os.path.exists(_NIX_PY) and os.environ.get("TKINTER_READY") != "1":
     os.environ["TKINTER_READY"] = "1"
     os.execv(_NIX_PY, [_NIX_PY] + sys.argv)
 
+import turtle
+import random
 
-import turtle, random
+screen = turtle.Screen()
+screen.title("Keyboard Drawing")
+screen.bgcolor("white")
 
-turtle.setup(600, 400)
-turtle.clearscreen()
-
-
-def say_score():
-  t.pendown()
-  score = 125
-  t.write("Score: " + str(score), font=("Arial", 16, "normal"))
+mike = turtle.Turtle()
+mike.speed(0)
+mike.width(1)
 
 def move_left():
-  t.pendown()
-  t.setheading(180)
-  t.forward(100)
-  
+    mike.setheading(180)
+    mike.forward(5)
 
-turtle.onkeypress(say_score, "a")
-turtle.onkeypress(move_left, "Left")
+def move_right():
+    mike.setheading(0)
+    mike.forward(5)
 
-turtle.listen()
-turtle.mainloop()
+def move_up():
+    mike.setheading(90)
+    mike.forward(5)
+
+def move_down():
+    mike.setheading(270)
+    mike.forward(5)
+
+def set_red():
+    mike.pencolor("red")
+
+def set_green():
+    mike.pencolor("green")
+
+def set_blue():
+    mike.pencolor("blue")
+
+# Step 3 - Random color
+
+def set_random_color():
+    colors = ["red", "blue", "green", "orange", "purple", "pink", "yellow", "cyan", "magenta", "coral"]
+    mike.pencolor(random.choice(colors))
+
+# Step 4a - Increase pen width
+
+def increase_width():
+    current = mike.width()
+    if current < 20:
+        mike.width(current + 1)
+
+def decrease_width():
+    current = mike.width()
+    if current > 1:
+        mike.width(current - 1)
+
+# Bind keys
+screen.listen()
+
+screen.onkeypress(move_left, "Left")
+screen.onkeypress(move_right, "Right")
+screen.onkeypress(move_up, "Up")
+screen.onkeypress(move_down, "Down")
+
+screen.onkeypress(set_red, "r")
+screen.onkeypress(set_green, "g")
+screen.onkeypress(set_blue, "b")
+
+screen.onkeypress(set_random_color, "c")
+
+screen.onkeypress(increase_width, "z")
+screen.onkeypress(decrease_width, "x")
+
+screen.mainloop()
