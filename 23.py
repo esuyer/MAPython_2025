9b3c33cea63e2e880ccefa133f3e3ff8ecb691d0
mangosigmacelo-netizen <mangosigmacelo@gmail.com>
2026-04-24 00:12:27 +0000

    cw

--- 23.py
+++ 23.py
@@ -1,14 +1,21 @@
+
+
 import turtle
 
 wn = turtle.Screen()
 wn.title("Mouse Click Counter")
+
+writer = turtle.Turtle()
+writer.hideturtle()
+writer.penup()
 
 click_count = 0
 
 def handle_click(x, y):
     global click_count
     click_count += 1
-    print(click_count)
+    writer.clear()
+    writer.write(click_count, align="center", font=("Arial", 24, "normal"))
 
 wn.onscreenclick(handle_click, 1)
 wn.listen()
