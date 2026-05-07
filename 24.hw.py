306cd72039be5bfd95a39134fdd2ac3cdc04c057
mangosigmacelo-netizen <mangosigmacelo@gmail.com>
2026-05-07 23:19:54 +0000

    HWWW

---
+++ 24.hw.py
@@ -0,0 +1,6 @@
+scores = {'Marie': 135, 'Pete': 150, 'Kevin': 120, 'Ann': 100}
+print("Before:", scores)
+scores['Marie'] = 120
+scores['Jake'] = 105
+scores['Kevin'] = scores['Kevin'] + 10
+print("After :", scores)
