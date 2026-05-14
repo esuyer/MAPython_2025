3a57dc95cf7df5387bf6a1b3c94b63f3bc7499f0
BCNHG <benmarjault@gmail.com>
2026-05-14 22:49:49 +0000

    Hw 25

--- Classwork.py
+++ Classwork.py
@@ -1,36 +1,79 @@
-pets= {"Cat": 100,
-      "Dog":150,
-      "Rabbit":15,
-      "Fish":10,
-      "Pig":5}
-print(pets)
-pets["Hampster"] = 35
-print(pets)
-a = int(input("How many more votes did the pig get?: "))
-pets["Pig"] = pets["Pig"] + a
-print(pets)
-for key in pets:
-  print(key)
-for key in pets:
-  print(pets[key])
-for key in pets:
-  print(key, " was chosen by ", pets[key]," people.")
-for key in pets:
-  if key == "Cat":
-    print(pets[key], " people chose cat as thier favorite animal.")
-b = pets["Cat"]
-c = pets["Dog"]
-d = pets["Rabbit"]
-e = pets["Fish"]
-f = pets["Pig"]
-g = pets["Hampster"]
-print("The total people who voted is ", int(b) + int(c) + int(d) + int(e) + int(f) + int(g),)
-h = pets["Dog"]
-i = pets["Cat"]
-if  int(h) > int(i):
-  print("The dog is the favorite animal by ", int(h) - int(i), " people.")
+'''
+print("Problem 1\n")
+names = []
+name = input("Enter multiple names separated by spaces: ")
+names = name.split()
+if names[1] == "Bob":
+  print("Bob is the second name")
 else:
-   print("The cat is the favorite animal by ", int(i) - int(h), " people.")
-for key in pets:
-   if pets[key] < 30:
-      print(key, pets[key])
+  print("Bob is not the second name")
+'''
+'''
+print("\nProblem 2\n")
+numbers = input("Enter a list of numbers separated by commas: ")
+numbers = numbers.split(",")
+print(int(numbers[0]) + int(numbers[-1]))
+'''
+'''
+print("\nProblem 3\n")
+text = input("Enter a list of strings separated by commas: ")
+def process_string(text):
+  words = text.split(',')
+  print(len(words), "words")
+  for word in words:
+      print(word)
+process_string(text)
+'''
+'''
+print("\nProblem 4\n")
+More = input("Enter a list of numbers separated by commas: ")
+Smore = More.split(",")
+for i in range(len(Smore)):
+  x = int(Smore[i])
+  if x < 10:
+    print(Smore[i])
+'''
+'''
+print("\nProblem 5\n")
+x = input("Enter 3 numbers: ")
+def first(x):
+  y = x.split(" ")
+  print(y)
+  if int(y[0]) + int(y[1]) == int(y[-1]):
+    print("True")
+  else:
+    print("False")
+first(x)
+'''
+'''
+print("\nProblem 6\n")
+evenmore = input("Enter a list of numbers: ")
+evenmore = evenmore.split(" ")
+evenmores = 0
+for i in range(len(evenmore)):
+  evenmores = evenmores + int(evenmore[i])
+print(evenmores)
+'''
+'''
+print("\nProblem 7\n")
+expression = input("Enter a operation followed by two numbers: ")
+def compute(expression):
+  parts = expression.split(' ')
+  symbol = parts[0]
+  num1 = int(parts[1])
+  num2 = int(parts[2])
+
+  if symbol == '+':
+      return num1 + num2
+  else:
+      return num1 - num2
+print(compute(expression))
+'''
+print("\nProblem 8\n")
+text = input("Give me several words separated by spaces: ")
+words = text.split(' ')
+for word in words:
+    if len(word) > 0:
+        if word[0].lower() == word[-1].lower():
+            print(word)
+
--- Hw.py
+++ Hw.py
@@ -1,61 +1,34 @@
-print("Problem 1\n")
-flavor = {"Marie":"Vanilla",
-          "Pete":"Chocolate",
-          "Kevin":"Strawberry",
-          "Ann":"Blueberry",
-          "Jake":"Rasberry"}
-scores = {"Marie":135,
-          "Pete":150,
-          "Kevin":120,
-          "Ann":100}
-print(scores)
-scores["Marie"] = 120
-scores["Jake"] = 105
-scores["Kevin"] = scores["Kevin"] + 10
-print(scores)
-
-print("\nProblem 2\n")
-a = input("Enter a name: ")
-if a in flavor:
-  print(a, "favorite ice cream flavor is ", flavor[a])
+words = input("Give me several words separated by spaces: ").split()
+print(words)
+if "rain" in words:
+    print("Found rain")
 else:
-  print("No information is avalible for that name.")
-b = input("What's your favorite flavor? ")
-for a in flavor:
-  if b in flavor[a]:
-    c = 0
-    c = c + 1
-print("Number of people that like", b, " ice cream: ", c)
+    print("Did not find rain")
 
-print("\nProblem 3\n")
-place = people = {
-  "Andre": "France",
-  "Jake": "USA",
-  "Ivan": "Russia",
-  "Maria": "Canada",
-  "Pete": "USA"}
-d = input("Which country do you live in? ")
-print("People who also live in ", d)
-for a in place:
-  if d in place[a]:
-    print(a)
+nums = input("Give me several integers separated by commas: ").split(",")
+for n in nums:
+    n = int(n)
+    print("+" * n)
 
-print("\n Problem 4\n")
-def test_passed(w, x, y, z):
-  total = w + x + y + z
-  if total < 360:
-      return False
-  else:
-      return True
-print(test_passed(100, 95, 90, 90)) 
-print(test_passed(100, 85, 100, 100))
-print(test_passed(90, 90, 89, 90))
-print(test_passed(90, 90, 90, 90))
+def robot(command):
+    parts = command.split()
+    word = parts[0]
+    times = int(parts[1])
+    for _ in range(times):
+        print(word)
 
-print("\nProblem 5\n")
-years = {'Joe': 1, 'Kevin': 99, 'Annie': 30, 'Pete': 15} 
-x = int(input("How many years have passed?"))
-print(x, "years passed: ")
-for key in  years:
-  years[key] = years[key] + x
-print(years)
+s = input("Give me a string: ")
+freq = {}
+for c in s:
+    if c not in freq:
+        freq[c] = 1
+    else:
+        freq[c] += 1
+print(freq)
+
+def get_password(message):
+    words = message.split()
+    password = ""
+    for w in words:
+        password += w[0]
+    return password
