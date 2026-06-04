8f0e86d759bc60ad4aa5b4581d11d069f4d26597
Ed Suyer <62613484+esuyer@users.noreply.github.com>
2026-06-04 22:53:55 +0000

    s

--- 23.py
+++ 23.py
@@ -1,4 +1,9 @@
+import sys, os
 
+_NIX_PY = "/nix/store/pjn7rx1d3jnjsif6wnz5im0fdm8pp7xa-python3-3.11.13/bin/python3"
+if os.path.exists(_NIX_PY) and os.environ.get("TKINTER_READY") != "1":
+    os.environ["TKINTER_READY"] = "1"
+    os.execv(_NIX_PY, [_NIX_PY] + sys.argv)
 
 import turtle
 
@@ -11,11 +16,13 @@ writer.penup()
 
 click_count = 0
 
+
 def handle_click(x, y):
     global click_count
     click_count += 1
     writer.clear()
     writer.write(click_count, align="center", font=("Arial", 24, "normal"))
+
 
 wn.onscreenclick(handle_click, 1)
 wn.listen()
