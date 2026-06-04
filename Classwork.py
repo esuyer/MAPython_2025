734e09d087759bcffc6718294f5d48ab2879f802
BCNHG <benmarjault@gmail.com>
2026-06-04 22:28:39 +0000

    Second one

--- Classwork.py
+++ Classwork.py
@@ -1,79 +1,18 @@
-'''
-print("Problem 1\n")
-names = []
-name = input("Enter multiple names separated by spaces: ")
-names = name.split()
-if names[1] == "Bob":
-  print("Bob is the second name")
-else:
-  print("Bob is not the second name")
-'''
-'''
-print("\nProblem 2\n")
-numbers = input("Enter a list of numbers separated by commas: ")
-numbers = numbers.split(",")
-print(int(numbers[0]) + int(numbers[-1]))
-'''
-'''
-print("\nProblem 3\n")
-text = input("Enter a list of strings separated by commas: ")
-def process_string(text):
-  words = text.split(',')
-  print(len(words), "words")
-  for word in words:
-      print(word)
-process_string(text)
-'''
-'''
-print("\nProblem 4\n")
-More = input("Enter a list of numbers separated by commas: ")
-Smore = More.split(",")
-for i in range(len(Smore)):
-  x = int(Smore[i])
-  if x < 10:
-    print(Smore[i])
-'''
-'''
-print("\nProblem 5\n")
-x = input("Enter 3 numbers: ")
-def first(x):
-  y = x.split(" ")
-  print(y)
-  if int(y[0]) + int(y[1]) == int(y[-1]):
-    print("True")
-  else:
-    print("False")
-first(x)
-'''
-'''
-print("\nProblem 6\n")
-evenmore = input("Enter a list of numbers: ")
-evenmore = evenmore.split(" ")
-evenmores = 0
-for i in range(len(evenmore)):
-  evenmores = evenmores + int(evenmore[i])
-print(evenmores)
-'''
-'''
-print("\nProblem 7\n")
-expression = input("Enter a operation followed by two numbers: ")
-def compute(expression):
-  parts = expression.split(' ')
-  symbol = parts[0]
-  num1 = int(parts[1])
-  num2 = int(parts[2])
-
-  if symbol == '+':
-      return num1 + num2
-  else:
-      return num1 - num2
-print(compute(expression))
-'''
-print("\nProblem 8\n")
-text = input("Give me several words separated by spaces: ")
-words = text.split(' ')
-for word in words:
-    if len(word) > 0:
-        if word[0].lower() == word[-1].lower():
-            print(word)
-
+import turtle
+import random
+import time
+screen = turtle.Screen()
+color = ["red", "green", "blue", "yellow", "purple", "orange"]
+def draw_circle_with_stamp():
+   time.sleep(500)
+   t = turtle.Turtle()
+   t.shape("circle")
+   t.color(random.choice(color))
+   t.stamp()
+   t.penup()
+   t.goto(random.randint(-200, 200), random.randint(-200, 200))
+   t.pendown()
+   t.circle(random.randint(10, 50))
+for i in range(random.randint(10,100)):
+ draw_circle_with_stamp()
+screen.mainloop()
--- Hw.py
+++ Hw.py
@@ -1,34 +1,65 @@
-words = input("Give me several words separated by spaces: ").split()
-print(words)
-if "rain" in words:
-    print("Found rain")
-else:
-    print("Did not find rain")
+def remove_short(words):
+    i = 0
+    while i < len(words):
+        if len(words[i]) < 5:
+            words.pop(i)
+        else:
+            i += 1
+    return words
 
-nums = input("Give me several integers separated by commas: ").split(",")
-for n in nums:
-    n = int(n)
-    print("+" * n)
-
-def robot(command):
-    parts = command.split()
-    word = parts[0]
-    times = int(parts[1])
-    for _ in range(times):
-        print(word)
-
-s = input("Give me a string: ")
-freq = {}
-for c in s:
-    if c not in freq:
-        freq[c] = 1
+def alarm(day, vacation):
+    if vacation:
+        if day == 1 or day == 7:
+            return "off"
+        else:
+            return "10:00"
     else:
-        freq[c] += 1
-print(freq)
+        if day == 1 or day == 7:
+            return "10:00"
+        else:
+            return "7:00"
 
-def get_password(message):
-    words = message.split()
-    password = ""
-    for w in words:
-        password += w[0]
-    return password
+def repeat_sep(word, separator, n):
+    result = ""
+    for i in range(n):
+        result += word
+        if i < n - 1:
+            result += separator
+    return result
+
+def neighbors(names):
+    for i in range(1, len(names) - 1):
+        print(names[i], "live between", names[i - 1], "and", names[i + 1])
+
+def fancy_string(nums):
+    result = ""
+    for i in range(len(nums) - 1):
+        if nums[i] < nums[i + 1]:
+            result += "<"
+        elif nums[i] > nums[i + 1]:
+            result += ">"
+        else:
+            result += "="
+    return result
+
+print(remove_short(['table', 'book', 'at', 'cupboard']))
+print(remove_short(['I', 'like', 'cake']))
+
+print(alarm(2, False))
+print(alarm(6, False))
+print(alarm(1, False))
+print(alarm(2, True))
+print(alarm(7, True))
+
+print(repeat_sep('This', 'And', 3))
+print(repeat_sep('That', 'Or', 1))
+print(repeat_sep('Ha', '-', 5))
+print(repeat_sep('Ha', '-', 0))
+
+neighbors(['Jacksons', 'Smiths', 'Foremans', 'Blooms', 'Lees'])
+neighbors(['Simpsons', 'Millers', 'Potters'])
+neighbors(['Johnsons', 'Bensons'])
+
+print(fancy_string([3, 3, 3, 3]))
+print(fancy_string([0, 1, 2, 3, 4, 5]))
+print(fancy_string([3, 3, 10, -5, 2]))
